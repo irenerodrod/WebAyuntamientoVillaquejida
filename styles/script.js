4cca134d441c1367df58369fcf3eb7f6a7a2f18b
Irene Rodríguez Rodríguez <irenerodrod@users.noreply.github.com>
2024-12-16 00:40:51 +0100

    Create script.js
    JS perteneciente a la barra lateral izquierda de la web

---
+++ styles/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('show'); // Toggling the "show" class to slide the menu in/out
+});
